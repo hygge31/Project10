d1d40b23de8b51db061665d34d5ac51a91370cbb
hygge31 <yiuter1@hanmail.net>
2024-02-18 19:18:20 +0900

    Feat. mouse Click movemonet

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+
+
+
+[RequireComponent(typeof(PlayerMovement))]
 public class PlayerController : MonoBehaviour
 {
+    public NavMeshAgent navMeshAgent;
     
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+
+
+
+  
+
+
+  
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+
+    PlayerController playerController;
+    public LayerMask clickableLayerMask;
+    public int clickableLayerMaskCode;
+
+    private void Awake()
+    {
+        clickableLayerMaskCode = LayerMask.GetMask("Ground");
+    }
+
+
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+
+
+    public void ClickToMove()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayerMask))
+        {
+            if(clickableLayerMask == clickableLayerMaskCode)
+            {
+                playerController.navMeshAgent.destination = hit.point;
+                RotateForward(hit.point);
+            }
+           
+        }
+    }
+
+
+    void RotateForward(Vector3 point)
+    {
+        Vector3 dir = (point - transform.position).normalized;
+        transform.forward = dir;
+    }
+}
